1e28d20274b532d6338a23acf3dbfca5aab4ef84
junibar <junibar@naver.com>
2025-04-29 17:49:13 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -76,11 +76,11 @@ async function generateQuestion(id, prompt) {
 }
 
 function loadAllQuestions() {
-  generateQuestion("q3-text", prompts.question3);
-  generateQuestion("q4-text", prompts.question4);
-  generateQuestion("q5-text", prompts.question5);
-  generateQuestion("q6-text", prompts.question6);
-  generateQuestion("q7-text", prompts.question7);
+  generateQuestion("q3-text", prompts.q3);
+  generateQuestion("q4-text", prompts.q4);
+  generateQuestion("q5-text", prompts.q5);
+  generateQuestion("q6-text", prompts.q6);
+  generateQuestion("q7-text", prompts.q7);
 }
 
 function nextScreen(nextId) {
