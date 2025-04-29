b3768c4ad3af589dbfc3c05458846a955203bfa6
junibar <junibar@naver.com>
2025-04-29 18:22:39 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "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";
 const prompts = {
   q3: `당신은 미술관 상설 체험 프로그램에서 관람객의 감상과 생각을 자연스럽게 끌어내는 생성형 AI입니다. 변상환 작가의 작업 키워드를 바탕으로 매번 다양한 감상 질문을 생성하세요.
 
