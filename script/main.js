994aea3fd3d9b4ba2a1998a0df799c58a742f67b
junibar <junibar@naver.com>
2025-04-30 10:14:56 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 
-const BASE_API_URL = "https://thinkspaceapi-8wewt5sgu-junibars-projects.vercel.app/api/gpt";
+const BASE_API_URL = "think-git-main-junibars-projects.vercel.app/api/gpt";
 const prompts = {
   q3: `당신은 미술관 상설 체험 프로그램에서 관람객의 감상과 생각을 자연스럽게 끌어내는 생성형 AI입니다. 변상환 작가의 작업 키워드를 바탕으로 매번 다양한 감상 질문을 생성하세요.
 
